37537ed35235ba5daf473fc83db55b1ade7c61f4
Tzikas <niko.tzikas@gmail.com>
2018-10-30 14:41:54 -0400

    state & stateful component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import logo from './logo.svg';
 import './App.css';
 
 class Dashboard extends Component {
+    state = {  //STEP 2 - state & stateful component 
+          todos: []
+    }
+
     componentDidMount(){ //STEP 1 - lifecyle events & components 
         console.log( ' this ',  this )
     }
