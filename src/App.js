c37d91958818415dc4b7130781552d0367814d37
Tzikas <niko.tzikas@gmail.com>
2018-10-30 14:42:38 -0400

    axios & serverURL

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'  //STEP 3  
+import {serverURL} from './config'
 
 class Dashboard extends Component {
     state = {  //STEP 2 - state & stateful component 
