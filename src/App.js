2095cd217dfabce6292d08cf0f1af7b8db04e387
Tzikas <niko.tzikas@gmail.com>
2018-10-30 15:37:30 -0400

    creating stateless component & inheriting props

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import axios from 'axios'  
 import {serverURL} from './config'
+import Todos from './Todos.js'
 
 class Dashboard extends Component {
     state = {  
@@ -15,12 +16,18 @@ class Dashboard extends Component {
 
     getTasks = async () => {       
         let getData = await axios.get(`${serverURL}/api/tasks`)   
-        this.setState({todos:getData.data}) //STEP 5 - setState        
+        this.setState({todos:getData.data})      
     }
 
     render() {
         return (
             <div className="App">
+                <h1>Todos</h1>                
+                {/*STEP 6 - passing state to props & importing components*/}
+                <Todos  
+                    color="cornflowerblue"
+                    todos={this.state.todos}
+                /> 
             </div>
         );
     }
---
+++ src/Todos.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+const Todos = (props) => {  //STEP 6 - creating stateless component & inheriting props 
+
+    return (
+        <div style={{color:props.color}}>            
+            <hr />
+            List: 
+        </div>
+    )
+}
+
+
+export default Todos
