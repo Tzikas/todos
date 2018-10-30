fcbb3c6f1990bb657a0e8af933ef4fcc8fc40c89
Tzikas <niko.tzikas@gmail.com>
2018-10-30 17:55:29 -0400

    added post function and converted stateless component to stateful

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ import {
 class Dashboard extends Component {
     state = {  
         todos: [],
-        loggedIn: true,
+        loggedIn: false,
         status: { error:false, message:'' },
         name: null,
         pass: null,
@@ -30,7 +30,8 @@ class Dashboard extends Component {
         
     }
     componentDidMount(){  
-        this.getTasks()        
+        this.getTasks()
+        this.loggedIn()        
     }
 
     getTasks = async () => {       
@@ -41,24 +42,29 @@ class Dashboard extends Component {
         let deleteData = await axios.post(`${serverURL}/api/tasks/delete/${id}`)   
         this.setState({todos: this.state.todos.filter(t => t._id !== id)})         
     }
+    postTask = async (value) => { //Add post action to component 
+        value.doneyet = false;
+        let todo = await postTask(value) 
+        this.setState({ todos: [...this.state.todos, todo] }) //Add new post to array of posts 
+    }
 
-    logIn = async() => { //Add imported functions to state  
-      let result = await logIn({ username:this.state.name, password:this.state.pass })
-      result.error ? this.setState({status: result } ) :  this.setState({user: result, loggedIn:true, status:{ error:false, message:'' } })    
+    logIn = async() => { 
+        let result = await logIn({ username:this.state.name, password:this.state.pass })
+        result.error ? this.setState({status: result } ) :  this.setState({user: result, loggedIn:true, status:{ error:false, message:'' } })    
     }
 
     signUp = async() => {
-      let result = await signUp({ username:this.state.name, password:this.state.pass })
-      result.error ? this.setState({status: result } ) :  this.setState({user: result, loggedIn:true, status:{ error:false, message:'' } }) 
+        let result = await signUp({ username:this.state.name, password:this.state.pass })
+        result.error ? this.setState({status: result } ) :  this.setState({user: result, loggedIn:true, status:{ error:false, message:'' } }) 
     }
 
     logOut = async() => {
-      await logOut()
-      this.setState({loggedIn:false, user:null, name:'', pass:'' })      
+        await logOut()
+        this.setState({loggedIn:false, user:null, name:'', pass:'' })      
     }
     loggedIn = async() => {
-      let result = await loggedIn()
-      result.error ? this.setState({status: result } ) :  this.setState({user: result, loggedIn:true, status:{ error:false, message:'' } }) 
+        let result = await loggedIn()
+        result.error ? this.setState({status: result } ) :  this.setState({user: result, loggedIn:true, status:{ error:false, message:'' } }) 
     }
 
 
@@ -68,7 +74,6 @@ class Dashboard extends Component {
                 <h1>Todos</h1>                                
 
                 <div className="App-content">
-                {/*Use the new fields in the state to conditonally show html*/}
                 {this.state.status.error ? <p> { this.state.status.message }</p> : ''}
                 {this.state.loggedIn && !this.state.status.error ? 
                   <span>
@@ -78,7 +83,8 @@ class Dashboard extends Component {
                         color="cornflowerblue"
                         todos={this.state.todos}
                         deleteTask={this.deleteTask}
-                  />
+                        postTask={this.postTask}                         
+                    /> {/*pass postTask as prop*/} 
                   </span>
                 : <div>
                     <p>Not logged in...</p>
@@ -90,13 +96,11 @@ class Dashboard extends Component {
                         />
                     </div>
                     <div> 
-                        {/*Use ref and onChange events to chage state*/}
                         <input 
                         type="password"                    
                         ref={pass => this.pass = pass} 
                         onChange = {() => this.setState({pass:this.pass.value})}/>                                
                     </div>
-                    {/*Use functions added to state*/}
                     <button onClick ={this.logIn}>LogIn</button> 
                     <button onClick ={this.signUp}>signUp</button>             
                 </div>
--- src/Todos.js
+++ src/Todos.js
@@ -1,4 +1,57 @@
 import React, { Component } from 'react';
+//import './todo.css';
+import axios from 'axios'
+
+//import TodoItem from './TodoItem';
+
+class Todos extends Component {
+
+    handleSubmit = async (e) => {        
+        e.preventDefault();
+        let value = this.input.value;
+        this.props.postTask({description:value})
+        this.input.value = ''     
+    }
+    
+    render() {
+        let todos = this.props.todos.map(todo => (
+            <li key={todo._id}> 
+                {todo.description}
+                <button onClick={() => this.props.deleteTask(todo._id)}>Delete</button>
+            </li>
+        )) 
+
+        return (
+            <div>
+                <h1 className="site-header">Todos</h1>
+                <div className="row">
+                    <div>
+                    <form onSubmit={this.handleSubmit}>
+                        <div>
+                            <input
+                                id="input-title"
+                                className="input-title"
+                                type="text"
+                                autoComplete="off"
+                                ref={input => {this.input = input}}
+                                placeholder="Add a todo..."/>
+                        </div>
+                    </form>
+
+                    <ul style={{color:this.props.color}}>
+                        {todos}
+                    </ul>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Todos;
+
+
+/*import React, { Component } from 'react';
 
 const Todos = (props) => {   
 
@@ -22,4 +75,4 @@ const Todos = (props) => {
 
 
 export default Todos
-
+*/
